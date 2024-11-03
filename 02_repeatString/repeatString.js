b03150537004dfb39b1c7d15a29b8a16527bedd3
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-03 12:16:42 +0100

    Finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let resultString = "";
 
+  if (num < 0) {
+    resultString = "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    resultString += string;
+  }
+
+  return resultString;
 };
 
 // Do not edit below this line

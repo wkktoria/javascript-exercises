ee0c8569eff2519fc32b5e8a60e28feeae708303
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-04 13:34:35 +0100

    Finish exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  const tempInCelsius = ((tempInFahrenheit - 32) * 5) / 9;
+  return Math.round(tempInCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  const tempInFahrenheit = (tempInCelsius * 9) / 5 + 32;
+  return Math.round(tempInFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

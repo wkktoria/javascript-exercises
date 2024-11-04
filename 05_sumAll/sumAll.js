81b376baf8142838e5fb34c6bddd93201e2b4f5c
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-04 13:20:07 +0100

    Finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
 
+  if (min < 0 || max < 0 || !Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
